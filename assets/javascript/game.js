0d512ff91cbbb63dc15ef81e93470a522077b3a4
Jieun Rim <rim.jieun@gmail.com>
2017-02-27 02:02:05 -0500

    worked on javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,50 @@
 $(document).ready(function() {
 
+	//-------------------------------Create Start Button----------------------------------------
+	$("#gameSection").html("<button type='button' onclick='startGame()'>Start</button>");
 
-var button = '<button type="button" onclick="startGame()">Start</button>';
+	var button = $("button");
 
-$("#gameSection").html(button);
+	button.css({"background-color":"red", "width":"200", "height":"80", "font-size":"30px"});
 
-$("#gameSection button").click(startGame());
+	// button.click(startGame); // start game when start button is clicked
+
+startGame();
+
+	//-------------------------------Create game functions--------------------------------------
+	function startGame() {
+
+		// THINGS I NEED:
+				// page with questions & answer choices
+				// DONE button
+				// finish page
+
+		var questionAnswers = [
+			{question: "Q1", answers: ["A", "B", "C", "D"]},
+			{question: "Q2", answers: ["A", "B", "C", "D"]},
+			{question: "Q3", answers: ["A", "B", "C", "D"]},
+			{question: "Q4", answers: ["A", "B", "C", "D"]},
+			{question: "Q5", answers: ["A", "B", "C", "D"]},
+			{question: "Q6", answers: ["A", "B", "C", "D"]}
+		];
 
 
-function startGame() {
+		for (i = 0; i < questionAnswers.length; i++) {
+			$("#gameSection").append("<p id='question'>" + questionAnswers[i].question + "</p>");
+			$("#gameSection").append("<p id='answers'>" + questionAnswers[i].answers + "</p>");
+			console.log(questionAnswers[i].question);	
 
-	alert("starting game");
 
 
+		}
 
-};
+
+		
+
+
+		
+
+	};
 
 
 
