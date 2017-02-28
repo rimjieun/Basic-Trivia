86f3e1f10a7eb1943fa8adaa52c11fee1f15896b
Jieun Rim <rim.jieun@gmail.com>
2017-02-28 01:07:00 -0500

    more js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,90 @@
 $(document).ready(function() {
 
-	//-------------------------------Create Start Button----------------------------------------
-	$("#gameSection").html("<button type='button' onclick='startGame()'>Start</button>");
+	var correct = 0;
+	var incorrect = 0;
+	var unanswered = 0;
+	var selectedAnswers = [];
+	var correctAnswers = [];
 
-	var button = $("button");
+	var questionAnswers = [
+			{question: "Q1", choices: ["A", "B", "C", "D"], answer: "C"},
+			{question: "Q2", choices: ["A", "B", "C", "D"], answer: "B"},
+			{question: "Q3", choices: ["A", "B", "hello", "D"], answer: "A"},
+			{question: "Q4", choices: ["A", "B", "C", "D"], answer: "C"},
+			{question: "Q5", choices: ["A", "B", "C", "D"], answer: "D"},
+			{question: "Q6", choices: ["A", "B", "C", "D"], answer: "D"}
+		];
 
-	button.css({"background-color":"red", "width":"200", "height":"80", "font-size":"30px"});
-
-	// button.click(startGame); // start game when start button is clicked
-
-startGame();
-
-	//-------------------------------Create game functions--------------------------------------
-	function startGame() {
-
-		// THINGS I NEED:
+// THINGS I NEED:
 				// page with questions & answer choices
 				// DONE button
 				// finish page
+				// var for correct, incorrect, unanswered
 
-		var questionAnswers = [
-			{question: "Q1", answers: ["A", "B", "C", "D"]},
-			{question: "Q2", answers: ["A", "B", "C", "D"]},
-			{question: "Q3", answers: ["A", "B", "C", "D"]},
-			{question: "Q4", answers: ["A", "B", "C", "D"]},
-			{question: "Q5", answers: ["A", "B", "C", "D"]},
-			{question: "Q6", answers: ["A", "B", "C", "D"]}
-		];
+	// startGame()
+	// No need to restart; will need to reload to play again
+	function startGame() {
 
-
+		// Creating the quiz
+		// Replace Start button with a new <div> to wrap quiz template
+		$("#gameSection").html("<div id='quiz'></div>");
+	
+		// 
 		for (i = 0; i < questionAnswers.length; i++) {
-			$("#gameSection").append("<p id='question'>" + questionAnswers[i].question + "</p>");
-			$("#gameSection").append("<p id='answers'>" + questionAnswers[i].answers + "</p>");
-			console.log(questionAnswers[i].question);	
-
-
-
+			$("#quiz").append("<p class='question' value=" + i + ">" + questionAnswers[i].question + "</p>");
+			$("#quiz").append("<form class='answerChoices' value=" + i + "></form>");
+			var choicesList = questionAnswers[i].choices;
+			for (j = 0; j < choicesList.length; j++) {
+				$("form[value=" + i + "]").append("<input type='radio' name='answerChoice' value=" + choicesList[j] + ">" + choicesList[j] + "</input>");
+			}
 		}
 
+		$("#gameSection").append("<button id='done-button'>Done</button>");
 
-		
+		// Store selected answers in array in order of questions-------------------------------------
+		$("input").on("change", function() {
+			var index = $(this).parent().attr("value");
+			selectedAnswers[index] = $(this).val();
+			console.log(selectedAnswers);
+		})
+		//-------------------------------------------------------------------------------------------
+
+		$("#done-button").on("click", function() {
+			checkAnswers(); // why can't I call this outside of startGame()?
+			console.log(correct);
+			console.log(incorrect);
+			console.log(unanswered);
+		})
+
+	}; // startGame ends
+
+	
+
+	function checkAnswers() {
+		for (i = 0; i < questionAnswers.length; i++) {
+			if (selectedAnswers[i] !== undefined) {
+				if (selectedAnswers[i] === questionAnswers[i].answer) {
+					correct++;
+				}
+				else {
+					incorrect++;
+				}
+			}
+			else {
+				unanswered++;
+			}
+		}
+	} //
+
+	
+
+	$("#start-button").on("click", function() {
+		startGame();
+	})
 
 
-		
 
-	};
-
+	
 
 
-
-})
+}) // document.ready ends
